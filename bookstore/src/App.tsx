35522a274f08aa31103652aae18273165772558b
Levente Hegedüs <levi.hegedus92@gmail.com>
2024-04-03 22:41:43 +0200

    modal added

--- bookstore/src/App.tsx
+++ bookstore/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Menu from "./components/Menu/Menu";
 import Releases from "./components/Releases/Releases";
 import Search from "./components/Search/Search";
 
 function App() {
   return (
     <>
+      <Menu />
       <Releases />
       <Search />
     </>
